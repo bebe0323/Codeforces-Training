31f4b6ae3987ad2ccf75284a29b56dcde8a563e6
Belgutei <z5458085@ad.unsw.edu.au>
2023-09-12 20:17:22 +1000

    redownloaded packages

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -19,8 +19,6 @@ export default function LoginPage() {
       headers: { 'Content-Type': 'application/json' },
       credentials: 'include',
     });
-    console.log('login resonse');
-    console.log(response);
     if (response.status === 200) {
       response.json()
         .then(data => {
