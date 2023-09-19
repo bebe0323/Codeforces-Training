0b22114756e16a5e2d514a17fc6f189136d17f68
Belgutei <z5458085@ad.unsw.edu.au>
2023-09-19 19:50:38 +1000

    added line chart

---
+++ client/src/problemList/FilterBox.js
@@ -0,0 +1,19 @@
+
+export function FilterBox({ handleSubmit, lower, upper, handleLower, handleUpper }) {
+  return (
+    <form className="filter-box" onSubmit={handleSubmit}>
+      <p className="filter-text">→ Filter Problems</p>
+      <div className="filter-input">
+        <p className="difficulty-text">Difficulty:</p>
+        <input value={lower} onChange={handleLower} className="difficulty-input" required />
+        -
+        <input value={upper} onChange={handleUpper} className="difficulty-input" required />
+      </div>
+      <div className="apply-button-div">
+        <button className="apply-button">
+          Apply
+        </button>
+      </div>
+    </form>
+  );
+}
