5db6a034c461c1fb81ae32553cf61a5c35057cc0
Belgutei <z5458085@ad.unsw.edu.au>
2023-09-19 17:10:39 +1000

    added filter by difficulty option on solved page

--- client/src/problemList/SolvedList.js
+++ client/src/problemList/SolvedList.js
@@ -26,6 +26,8 @@ export function findSolvedDuration(startedDate, finishedDate) {
 export default function SolvedList() {
   const [problemList, setProblemList] = useState(null);
   const [refresh, setRefresh] = useState(0);
+  const [lower, setLower] = useState('');
+  const [upper, setUpper] = useState('');
 
   useEffect(() => {
     // using async function here to avoid use async TodoList()
@@ -65,41 +67,76 @@ export default function SolvedList() {
         .then(error => console.log(error))
     }
   }
+
+  async function handleSubmit() {
+    
+    const response = await fetch(`${backendURL}/list/${'solved'}/${lower}/${upper}`, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+    });
+    if (response.status === 200) {
+      response.json()
+        .then(data => setProblemList(data))
+    } else {
+      response.json()
+        .then(data => alert(data))
+    }
+  }
+
+  function handleLower(e) { setLower(e.target.value); }
+  function handleUpper(e) { setUpper(e.target.value); }
   
   return (
     <div>
       <h1>Solved List</h1>
       {problemList !== null ? (
-        <table>
-          <thead>
-            <tr>
-              <th style={{width: '3.75em', }}>#</th>
-              <th style={{textAlign: 'center'}}>Name</th>
-              <th style={{width: '2.5em'}}>Difficulty</th>
-              <th>Date solved</th>
-              <th>Solved Duration</th>
-              <th>Note</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {problemList.map((item, index) => (
-              <tr key={index} className={!index % 2 ? 'active-row': ''}>
-                <td><a target="_blank" rel="noreferrer noopener" className="cfLink" href={item.link}>{item.problemId}</a></td>
-                <td>{item.title}</td>
-                <td>{item.difficulty}</td>
-                <td>{findDate(item.finishedDate)}</td>
-                <td>{findSolvedDuration(item.startedDate, item.finishedDate)}</td>
-                <td>{item.note}</td>
-                <td>
-                  <Button onClick={() => handleRemove(item.problemId)} variant="danger">
-                    Remove
-                  </Button>{' '}
-                </td>
+        <div className="solved-page">
+          <table className="solved-table">
+            <thead>
+              <tr>
+                <th style={{width: '3.75em', }}>#</th>
+                <th style={{textAlign: 'center'}}>Name</th>
+                <th style={{width: '2.5em'}}>Difficulty</th>
+                <th>Date solved</th>
+                <th>Solved Duration</th>
+                <th>Note</th>
+                <th>Actions</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {problemList.map((item, index) => (
+                <tr key={index} className={!index % 2 ? 'active-row': ''}>
+                  <td><a target="_blank" rel="noreferrer noopener" className="cfLink" href={item.link}>{item.problemId}</a></td>
+                  <td>{item.title}</td>
+                  <td>{item.difficulty}</td>
+                  <td>{findDate(item.finishedDate)}</td>
+                  <td>{findSolvedDuration(item.startedDate, item.finishedDate)}</td>
+                  <td>{item.note}</td>
+                  <td>
+                    <Button onClick={() => handleRemove(item.problemId)} variant="danger">
+                      Remove
+                    </Button>{' '}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <form className="filter-box" onSubmit={handleSubmit}>
+            <p className="filter-text">→ Filter Problems</p>
+            <div className="filter-input">
+              <p className="difficulty-text">Difficulty:</p>
+              <input value={lower} onChange={handleLower} className="difficulty-input" required/>
+              -
+              <input value={upper} onChange={handleUpper} className="difficulty-input" required/>
+            </div>
+            <div className="apply-button-div">
+              <button className="apply-button">
+                Apply
+              </button>
+            </div>
+          </form>
+        </div>
       ): (
         <>
           Loading
--- server/problem/getProblemsList.js
+++ server/problem/getProblemsList.js
@@ -1,12 +1,29 @@
 import { ProblemModel } from "../models/ProblemModel.js";
 
-export default async function getProblemsList(username, status) {
+
+export default async function getProblemsList(username, status, lower, upper) {
   try {
+    const lowerNum = parseInt(lower);
+    const upperNum = parseInt(upper);
+    if (isNaN(lowerNum) || isNaN(upperNum)) {
+      return { error: 'Difficulty must be a number' };
+    }
     const problems = await ProblemModel.find({
       username: username,
       status: status
     });
-    return problems;
+    const inRangeProblems = problems.filter(problem => {
+      const difficulty = problem.difficulty;
+      return lowerNum <= difficulty && difficulty <= upperNum;
+    });
+    let sortedProblems;
+    // sorting that the most recent added is on the top
+    if (status === 'todo' || status === 'skipped') {
+      sortedProblems = inRangeProblems.sort((a, b) => b.addedDate - a.addedDate);
+    } else {
+      sortedProblems = inRangeProblems.sort((a, b) => b.finishedDate - a.finishedDate);
+    }
+    return sortedProblems;
   } catch (error) {
     return { error: 'internal server error' };
   }
